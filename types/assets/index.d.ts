70bc1e79472fc11847ebc852e03702ebf3615be8
Steve Westhoek <info@prochat.tools>
2026-07-02 19:08:18 +0100

    fix: type static image asset imports in ci

---
+++ types/assets/index.d.ts
@@ -0,0 +1,13 @@
+declare module '*.svg' {
+  import type { StaticImageData } from 'next/image'
+
+  const content: StaticImageData
+  export default content
+}
+
+declare module '*.png' {
+  import type { StaticImageData } from 'next/image'
+
+  const content: StaticImageData
+  export default content
+}
